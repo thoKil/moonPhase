cc417f4a43500eb056a7562dec0ebfe9ace0fe23
Thorsten <studium@thorstenkilian.de>
2025-06-12 21:58:58 +0200

    Add images and css

--- main.go
+++ main.go
@@ -4,8 +4,15 @@
 * Return moon phase as json over API.
 * Show the moon phase on HTML website.
 *
+* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+* IMPORTANT: To run this application, call:
+* export PORT=8081 (or another port)
+* export API_KEY=33902b95ee204b5eb31165429251106
+* go run main.go
+* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+*
 * author: thokil
-* last update: 10.06.2025
+* last update: 12.06.2025
 ******************************************************************/
 
 package main
@@ -21,61 +28,152 @@ import (
 )
 
 type MoonPhase struct {
-	Phase        string  `json:"Phase"`
-	Illumination float64 `json:"Illumination"`
+	Phase        string `json:"Phase"`
+	Illumination int    `json:"Illumination"`
+	Image        string `json:"Image"`
 }
 
 func main() {
-	fmt.Println("Hello Moon")
-	//Call funnctions for this urls
 	http.HandleFunc("/api/moonphase", apiHandler)
 	http.HandleFunc("/", htmlHandler)
-	port := os.Getenv("Port")
+
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+	// export PORT=8081 (or another port)
+	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8081"
 	}
-	log.Printf("Der Mondphasen-Kalender läuft auf http://localhost:%s", port)
-	//Starts webserver on port 8081
-	http.ListenAndServe(":"+port, nil)
+	log.Printf("The moon-phase-App runs on http://localhost:%s", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
 func apiHandler(writer http.ResponseWriter, request *http.Request) {
-	//Get the date from the url (...?date=dd-mm-yyyy)
-	date := request.URL.Query().Get("date")
-	if date == "" {
-		date = time.Now().Format("10-06-2025")
-	}
-	//Get the Moon Phase
-	moon, err := getMoonPhase(date)
+	moon, err := fetchMoonData(request)
 	if err != nil {
 		http.Error(writer, "Error to get moon phase", http.StatusInternalServerError)
 		return
 	}
 	//return the moon phase as json
-	writer.Header().Set("ContentType", "application/json")
+	writer.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(writer).Encode(moon)
 }
 
 // show HTML page
 func htmlHandler(writer http.ResponseWriter, request *http.Request) {
-	date := time.Now().Format("10-06-2025")
-	moon, err := getMoonPhase(date)
+	moon, err := fetchMoonData(request)
 	if err != nil {
 		http.Error(writer, "Error to get moon phase", http.StatusInternalServerError)
 		return
 	}
 	template, err := template.ParseFiles("template.html")
 	if err != nil {
-		http.Error(writer, "Error on template", http.StatusInternalServerError)
+		http.Error(writer, "Error to load template", http.StatusInternalServerError)
 		return
 	}
-	template.Execute(writer, moon)
+	if err := template.Execute(writer, moon); err != nil {
+		log.Println("Template execute Error: ", err)
+	}
 }
 
-// return moon phase (Fakedata)
+func fetchMoonData(request *http.Request) (MoonPhase, error) {
+	date := request.URL.Query().Get("date")
+	if date == "" {
+		date = time.Now().Format("2006-01-02")
+	}
+	moon, err := getMoonPhase(date)
+	if err != nil {
+		return moon, err
+	}
+	moon.Phase = translatePhase(moon.Phase)
+	return moon, nil
+}
+
+// return moon phase
 func getMoonPhase(date string) (MoonPhase, error) {
+
+	// export API_KEY=33902b95ee204b5eb31165429251106
+	// go run main.go
+
+	apiKey := os.Getenv("API_KEY")
+	if apiKey == "" {
+		return MoonPhase{}, fmt.Errorf("Api Key not set")
+	}
+
+	url := fmt.Sprintf("https://api.weatherapi.com/v1/astronomy.json?key=%s&q=Solingen&dt=%s", apiKey, date)
+
+	response, err := http.Get(url)
+	if err != nil {
+		return MoonPhase{}, err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return MoonPhase{}, fmt.Errorf("API Error %s", response.Status)
+	}
+
+	var result struct {
+		Astronomy struct {
+			Astro struct {
+				MoonPhase        string `json:"moon_phase"`
+				MoonIllumination int    `json:"moon_illumination"`
+			} `json:"astro"`
+		} `json:"astronomy"`
+	}
+
+	if err := json.NewDecoder(response.Body).Decode(&result); err != nil {
+		return MoonPhase{}, err
+	}
+
 	return MoonPhase{
-		Phase:        "Full Moon",
-		Illumination: 100,
+		Phase:        result.Astronomy.Astro.MoonPhase,
+		Illumination: result.Astronomy.Astro.MoonIllumination,
+		Image:        getPhaseImage(result.Astronomy.Astro.MoonPhase),
 	}, nil
+}
+
+func translatePhase(phase string) string {
+	switch phase {
+	case "New Moon":
+		return "Neumond"
+	case "Waxing Crescent":
+		return "Zunehmender Sichelmond"
+	case "First Quarter":
+		return "Zunehmender Halbmond"
+	case "Waxing Gibbous":
+		return "Zunehmender Dreiviertelmond"
+	case "Full Moon":
+		return "Vollmond"
+	case "Waning Gibbous":
+		return "Abnehmender Dreiviertelmond"
+	case "Last Quarter":
+		return "Abnehmender Halbmond"
+	case "Waning Crescent":
+		return "Abnehmendert Sichelmond"
+	default:
+		return phase
+	}
+}
+
+func getPhaseImage(phase string) string {
+	switch phase {
+	case "New Moon":
+		return "/static/new_moon.png"
+	case "Waxing Crescent":
+		return "/static/waxing_crescent.png"
+	case "First Quarter":
+		return "/static/first_quarter.png"
+	case "Waxing Gibbous":
+		return "/static/waxing_gibbous.png"
+	case "Full Moon":
+		return "/static/full_moon.png"
+	case "Waning Gibbous":
+		return "/static/waning_gibbous.png"
+	case "Last Quarter":
+		return "/static/last_quarter.png"
+	case "Waning Crescent":
+		return "/static/waning_crescent.png"
+	default:
+		return ""
+	}
 }
